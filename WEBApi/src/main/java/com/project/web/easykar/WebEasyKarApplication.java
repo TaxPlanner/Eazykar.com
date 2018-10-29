32de8f7388f5c4ade240a96df34fbcd04d90ec24
nikhil0789 <nikhilgupta889@gmail.com>
2018-10-29 19:54:01 +0530

    Changes made by manoj

--- WEBApi/src/main/java/com/project/web/easykar/WebEasyKarApplication.java
+++ WEBApi/src/main/java/com/project/web/easykar/WebEasyKarApplication.java
@@ -4,11 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class WebEasyKarApplication extends SpringBootServletInitializer{
---
+++ WEBApi/src/main/java/com/project/web/easykar/constant/VariableConstant.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.project.web.easykar.constant;
+
+/**
+ *
+ * @author manoj
+ */
+public class VariableConstant {
+    public String BaseUrl="http://localhost:8080/rest/";
+    public String ApiLogin="users/login";
+    public String ApiRegistration="users/registeration";
+
+    public VariableConstant() {
+    }
+}
