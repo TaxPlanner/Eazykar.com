a904940baad1c050c57000ba58266c629842ef25
Vivek More <vivek.more@gfs.com>
2018-11-27 23:41:32 -0500

    [code-format] remove unnecessary package comments

--- easykar-rest/src/main/java/com/easykar/rest/repository/UserRepository.java
+++ easykar-rest/src/main/java/com/easykar/rest/repository/UserRepository.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.easykar.rest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
